28c6654da84c7392adb295743d989730d1fa4171
DiegoGallardoRP <diogallardo2004@gmail.com>
2024-11-06 12:43:28 +0100

    dia26

--- TV_WATCH/clases/Peliculas.js
+++
@@ -1,8 +0,0 @@
-class Pelicula{
-    constructor(titulo,director,categoria,foto){
-        this.titulo=titulo;
-        this.director=director;
-        this.categoria=categoria;
-        this.foto=foto;
-    }
-}
