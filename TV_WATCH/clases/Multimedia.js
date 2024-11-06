80e14e0f282d367ee511b73ae97f8e304d0e5e56
DiegoGallardoRP <150666404+DiegoGallardoRP@users.noreply.github.com>
2024-11-06 22:23:59 +0100

    Add files via upload

---
+++ TV_WATCH/clases/Multimedia.js
@@ -0,0 +1,11 @@
+class Multimedia{
+    constructor(tipo,titulo,director,categoria,foto,link){
+        //serie/pelicula
+        this.tipo=tipo;
+        this.titulo=titulo;
+        this.director=director;
+        this.categoria=categoria;
+        this.foto=foto;
+        this.link=link||"";
+    }
+}
