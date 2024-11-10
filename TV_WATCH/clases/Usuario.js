fa3ad5af7b1ace6d28ac1640e7be928c1444c254
DiegoGallardoRP <150666404+DiegoGallardoRP@users.noreply.github.com>
2024-11-10 21:31:43 +0100

    Add files via upload

--- TV_WATCH/clases/Usuario.js
+++ TV_WATCH/clases/Usuario.js
@@ -1,12 +1,47 @@
 class Usuario{
-    constructor(codigo,clave,nombre,correo,admin,estado){
+    constructor(codigo,clave,nombre,correo,tipoSub,fechaFin,admin){
         this.codigo=codigo;
         this.clave=clave;
         this.nombre=nombre;
         this.correo=correo;
+        this.tipoSub=tipoSub||"";
+        this.fechaFin=fechaFin||new Date();
         this.admin=admin||"N";
-        //S/N ---> SI/NO
-        this.estado=estado||"B";
-        //A/B ---> ALTA/BAJA
+        //S/N ---> SI/NO     
+    }
+    //Editar usuario
+    editarUsuario(nuevoCodigo, nuevoNombre, nuevoCorreo, nuevaClave, nuevoTipoSub, nuevaFechaFin, 
+        listaUsuarios){
+        this.codigo=nuevoCodigo;
+        this.nombre=nuevoNombre;
+        this.correo=nuevoCorreo;
+        this.clave=nuevaClave;
+        this.tipoSub=nuevoTipoSub;
+        this.fechaFin=nuevaFechaFin;
+
+        //buscar el índice del usuario
+        let indice=listaUsuarios.findIndex(usuario=>usuario.codigo==this.codigo);
+        if(indice!=-1){
+            listaUsuarios[indice]=this;
+            localStorage.setItem("listaUsuarios",JSON.stringify(listaUsuarios));
+            sessionStorage.setItem("usuarioActual",JSON.stringify(this));
+            return true;
+        }
+        return false;
+    }
+    //Mostrar información del usuario para editarlo
+    mostrarInformacion() {
+        document.getElementById("nombre").value = this.nombre;
+        document.getElementById("correo").value = this.correo;
+    }
+    //Comprobar fecha subscripcion
+    tieneSuscripcionActiva() {
+        let fechaActual = new Date();
+        let fechaUsuario = new Date(this.fechaFin);
+        if(fechaUsuario > fechaActual){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
